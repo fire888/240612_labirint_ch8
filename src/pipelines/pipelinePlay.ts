59e85447ca2985c03ddcbf8fef2c28eb4e3996c6
vasilii <porokh.vs@gmail.com>
2024-08-04 20:02:07 +0300

    feat[ORBIT]: rotate

--- src/pipelines/pipelinePlay.ts
+++ src/pipelines/pipelinePlay.ts
@@ -18,7 +18,6 @@ export const pipelinePlay = async (root: Root) => {
 
     controlsPointer.disable()
     controlsOrbit.enable()
-    //controlsOrbit.disable()
 
     const onKeyUp = (event: any) => {
         if (event.code === 'KeyO') {
