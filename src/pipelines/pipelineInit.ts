4c000d396f1ec48b39811b48842e94a42badd3e9
vasilii <porokh.vs@gmail.com>
2025-01-08 15:41:33 +0300

    style

--- src/pipelines/pipelineInit.ts
+++ src/pipelines/pipelineInit.ts
@@ -1,11 +1,6 @@
 import { Root } from '../index'
 import * as TWEEN from '@tweenjs/tween.js'
 
-import { createRandomDataForLine } from 'geometry/_lineGeom'
-import { createTileI } from 'geometry/tile_I'
-import { _M } from 'geometry/_m'
-import * as THREE from 'three'
-
 export const pipelineInit = async (root: Root) => {
     const {
         CONSTANTS,
@@ -33,7 +28,7 @@ export const pipelineInit = async (root: Root) => {
     })
 
     studio.init(root)
-    studio.addAxisHelper()
+    //studio.addAxisHelper()
     ticker.on(studio.render.bind(studio))
 
     ui.init(root)
@@ -68,29 +63,4 @@ export const pipelineInit = async (root: Root) => {
     audio.playAmbient()
     controls.init(root)
     ticker.on(controls.update.bind(controls))
-
-
-    {
-        // const r0 = createRandomDataForLine()
-        // const r1 = createRandomDataForLine()
-        // r1.color = [0, 0, 0]
-        // const el = createTileI({
-        //     forms: [r0.form, r1.form],
-        //     paths: [r0.path, r1.path],
-        //     colors: [r0.color, r1.color],
-        //     key: 'string',
-        //     n: 10,
-        //     w: 5,
-        // })
-        // _M.translateVertices(el.v, 0, 0, 3)
-        // const m = _M.createMesh({
-        //     v: el.v,
-        //     c: el.c,
-        //     material: new THREE.MeshPhongMaterial({ color: 0xffffff, vertexColors: true }),
-        // })
-        // studio.add(m)
-        // ticker.on(() => { 
-        //     m.rotation.y += 0.01    
-        // })
-    }
 }
